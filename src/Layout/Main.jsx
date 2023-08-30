2247812dcd754c33e76efd292d39d5ed86744492
ahad1033 <a.ahad2232@gmail.com>
2023-08-31 02:29:47 +0600

    code modified

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
-import Navbar from '../Component/Navbar/Navbar'
-import Footer from '../Component/Footer/Footer'
+import Navbar from '../Component/Shared/Navbar/Navbar'
+import Footer from '../Component/Shared/Footer/Footer'
 
 const Main = () => {
   return (
